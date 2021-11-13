172e9840110c2efa9b015730d93585696ceb207f
Adrian Ryszka <adrian.ryszka@gmx.net>
2021-11-13 18:44:45 +0100

    Project Setup

---
+++ backend/2FactorAuth/src/test/java/at/ar85/FactorAuth/ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ar85.FactorAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
